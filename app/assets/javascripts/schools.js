d88a3ae70a47889ed4a186a159d477fc9e965345
Arun Madisetty <arun.madisetty@gmail.com>
2017-09-07 19:55:07 -0500

    Implemented markers into Google Maps on savedsearch page

--- app/assets/javascripts/schools.js
+++ app/assets/javascripts/schools.js
@@ -1,4 +1,4 @@
-/* global Vue, Rails */
+/* global Vue, Rails, google */
 document.addEventListener("DOMContentLoaded", function(event) { 
   var app = new Vue({
     el: '#app',
@@ -11,6 +11,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
         type: "GET",
         success: function(data) {
           console.log(data);
+          var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 41.8922745, lng: -87.6346887},
+            zoom: 11
+          });
+          data.forEach(function(datum) {
+            console.log('datum is', datum["location"]["coordinates"]);
+            var latitude = datum["location"]["coordinates"][1];
+            var longitude = datum["location"]["coordinates"][0];
+            // var point = {lat: latitude, lng: longitude};
+            // console.log('point is ', point);
+            var marker = new google.maps.Marker({
+              map: map,
+              position: {lat: latitude, lng: longitude}
+            });
+          });
+
           this.savedsearch = data;
         }.bind(this)
       });
